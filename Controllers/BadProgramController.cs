9b8eed59816998344c41f2c84eb5cc312a78839e
Gowtham Raj <Gowthamraj.engg@gmail.com>
2022-07-06 03:39:06 +0530

    Add project files.

---
+++ Controllers/BadProgramController.cs
@@ -0,0 +1,22 @@
+﻿using AsynchronousWebApi.Processors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsynchronousWebApi.Controllers
+{
+    [ApiController]
+    public class BadProgramController : Controller
+    {
+        private readonly LegacyService legacyService;
+
+        public BadProgramController(LegacyService legacyService)
+        {
+            this.legacyService = legacyService;
+        }
+        [HttpGet("Hi")]
+        public IActionResult SyncOverAsyncResultV1()
+        {
+            string val = legacyService.DoAsyncOperationWell().Result;
+            return Ok(val);
+        }
+    }
+}
---
+++ Processors/LegacyService.cs
@@ -0,0 +1,12 @@
+﻿namespace AsynchronousWebApi.Processors
+{
+    public class LegacyService
+    {
+        public async Task<string> DoAsyncOperationWell()
+        {
+            var random = new Random();
+            await Task.Delay(random.Next(10) * 1000);
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
