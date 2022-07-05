cb59a52a5faf496550b8e1a3309d8de3a5e99d70
Gowtham Raj <Gowthamraj.engg@gmail.com>
2022-07-06 03:49:09 +0530

    Bad Asynchronous API's added

--- Controllers/BadProgramController.cs
+++ Controllers/BadProgramController.cs
@@ -18,5 +18,33 @@ namespace AsynchronousWebApi.Controllers
             string val = legacyService.DoAsyncOperationWell().Result;
             return Ok(val);
         }
+        [HttpGet("Lowcpu/uses-too-many-threadpool-threads-v2")]
+        public IActionResult SyncOverAsyncResultV2()
+        {
+            string val = Task.Run(() => legacyService.DoAsyncOperationWell()).GetAwaiter().GetResult();
+            return Ok(val);
+        }
+        [HttpGet("lowcpu/uses-too-many-threadpool-threads-v3")]
+        public IActionResult SyncOverAsyncResultV3()
+        {
+            string val = Task.Run(() => legacyService.DoAsyncOperationWell().GetAwaiter().GetResult()).GetAwaiter().GetResult();
+            return Ok(val);
+        }
+        /// <returns></returns>
+        [HttpGet("Lowcpu/uses-too-many-threadpool-threads-v4")]
+        public IActionResult SyncOverAsyncResultV4()
+        {
+            var task = legacyService.DoAsyncOperationWell();
+            task.Wait();
+            string val = task.GetAwaiter().GetResult();
+            return Ok(val);
+        }
+
+        [HttpGet("Lowcpu/uses-too-many-threadpool-threads-fixed")]
+        public async Task<IActionResult> SyncOverAsyncResultFixedAsync()
+        {
+            var result = await legacyService.DoAsyncOperationWell();
+            return Ok(result);
+        }
     }
 }
